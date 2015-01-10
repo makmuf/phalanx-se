cef4cdf8c322d9ffbf18c64bc602e1ee56eb15c7
bomberman1990 <son.levuthai@gmail.com>
2015-01-11 01:02:08 +0700

    var with configuration key

--- config/var.php
+++ config/var.php
@@ -6,6 +6,8 @@ return [
 
     'debug'     => false,
 
-    'name'      => 'Phalanx Standard Edition'
+    'name'      => 'Phalanx Standard Edition',
+
+    'key'       => '$2y$10$NobwhMg4QbYH9QzbGmp3ieLxOw05z.uS.qclq1AuuYgNJsTWmHkWu',
 
 ];
